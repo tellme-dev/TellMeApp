4139213b296da9c4f0ab3ac23fa57df377248a2e
xml777 <386002446@qq.com>
2015-11-12 15:41:37 +0800

    init 1

---
+++ TellMeApp/www/app.js
@@ -0,0 +1,7 @@
+﻿angular.module('tellme', ['ionic', 'ngIOS9UIWebViewPatch'])
+    .run(['$ionicPlatform', '$rootScope', function ($ionicPlatform, $rootScope) {
+        $ionicPlatform.ready(function () { })
+
+
+
+    }])
