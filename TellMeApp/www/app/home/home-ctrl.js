6f59ad856647fdc14a34fd0fcaadf24f6076ff95
SeeLittleGirlAgain <1309230548@qq.com>
2015-11-19 13:34:54 +0800

    1、高德地图定位

--- TellMeApp/www/app/home/home-ctrl.js
+++ TellMeApp/www/app/home/home-ctrl.js
@@ -1,6 +1,24 @@
 ﻿angular.module('tellme')
     .controller('homeControll', ['$scope', '$state', function ($scope, $state) {
         /*首页初始化*/
+        // 获取当前位置
+        $scope.getCity = function () {
+            //加载城市查询插件
+            AMap.service(["AMap.CitySearch"], function () {
+                //实例化城市查询类
+                var citysearch = new AMap.CitySearch();
+                //自动获取用户IP，返回当前城市
+                citysearch.getLocalCity(function (status, result) {
+                    if (status === 'complete' && result.info === 'OK') {
+                        if (result && result.city && result.bounds) {
+                            var cityinfo = result.city;
+                            console.log('当前城市：' + cityinfo);
+                        }
+                    }
+                });
+            });
+        }
+
         //广告（头部广告、底部专栏）动态加载
         //菜单先查询本地是否有保存，没有，动态加载；有，在家本地数据；
         //加载用户头像
@@ -8,11 +26,15 @@
 
         /*跳转“定位页面”*/
         $scope.goToLocation = function () {
-            $state.go('home');
+            $state.go('map/location');
         }
         /*跳转“个人信息页面”*/
         $scope.goToCustomer = function () {
-            $state.go('home');
+            $state.go('customer/center');
+        }
+        //跳转到搜索页面
+        $scope.goToSearch = function () {
+            $state.go('');
         }
         /*（点击菜单项）跳转“酒店列表”*/
         $scope.goToHotelList = function (param) {
