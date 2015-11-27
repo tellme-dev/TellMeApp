a3557a11492e0ea60ad4b4390e544798f813c533
18328623495 <604356827@qq.com>
2015-11-27 16:06:57 +0800

    添加文件

---
+++ TellMeApp/www/app/community/list/community-list-ctrl.js
@@ -0,0 +1,24 @@
+﻿angular.module('tellme')
+    .controller('communityControll', ['$scope', '$window', '$state','$ionicHistory',
+        function ($scope, $window, $state, $ionicHistory) {
+            /*返回前一个界面*/
+            $scope.$window = $window;
+            $scope.goBack = function () {
+                $ionicHistory.goback();
+                $window.history.back();
+            };
+            //跳转到首页
+            $scope.goHome = function () {
+                $state.go('home');
+            }
+            /*（点击底部菜单）跳转“发现”*/
+            $scope.goDiscover = function () {
+                $state.go('discoverList');
+            }
+            /*（点击底部菜单）跳转“入住”*/
+            $scope.goCheckinto = function () {
+                // $state.go('communityList');
+                console.log("跳转到入住");
+            }
+        }
+    ]);
