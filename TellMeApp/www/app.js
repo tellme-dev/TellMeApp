72386367cf74237465ff35f2acceafcda0bbaaff
18328623495 <604356827@qq.com>
2015-11-27 17:08:44 +0800

    页面整合

--- TellMeApp/www/app.js
+++ TellMeApp/www/app.js
@@ -46,6 +46,7 @@
         } else {
             $urlRouterProvider.otherwise('/start');
         }
+        //$urlRouterProvider.otherwise('/home');
         /*修改put 和 post 的数据传递方式*/
         $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
--- TellMeApp/www/app/community/community-list-ser.js
+++ TellMeApp/www/app/community/community-list-ser.js
@@ -1,1 +1,20 @@
-﻿
+﻿angular.module('tellme')
+    .service('communitySer', ['$http', '$q', 'appConfig', function ($http, $q, appConfig) {
+        var baseUrl = appConfig.server.getUrl();
+        //获取社区分类标签
+        this.getCommunityType = function () {
+            var url = baseUrl + 'app/bbs/loadBbsCategoryList.do';
+            var deferred = $q.defer();
+            $http({
+                method: 'post',
+                url: url
+            }).success(
+                function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(
+                function (data, status, headers, config) {
+                    deferred.reject(5);
+                });
+            return deferred.promise;
+          }
+    }]);
