3e690840ef503fa1d3af5dea1e483de744e11c1e
Liutaixiong <531457277@qq.com>
2015-12-01 17:32:25 +0800

    酒店列表菜单1/2级数据更新

--- TellMeApp/www/app/hotel/hotel-ser.js
+++ TellMeApp/www/app/hotel/hotel-ser.js
@@ -1,4 +1,68 @@
 ﻿angular.module('tellme')
     .service('hotelSer', ['$http', '$q', 'appConfig', function ($http, $q, appConfig) {
         var baseUrl = appConfig.server.getUrl();
+        this.hostUrl = baseUrl;
+
+        //获取1级菜单
+        this.getRootMenu = function () {
+            var url = baseUrl + 'app/menu/loadMenuRootList.do';
+            var getDataJSON = JSON.stringify({
+                position: "hotelList"
+            });
+            var deferred = $q.defer();
+            $http({
+                method: 'post',
+                url: url,
+                data: { json: getDataJSON }
+            }).success(
+                function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(
+                function (data, status, headers, config) {
+                    deferred.reject(5);
+                });
+            return deferred.promise;
+        }
+
+        //获取2级菜单
+        this.getChildMenu = function (itemTagId) {
+            var url = baseUrl + 'app/menu/loadMenuChildList.do';
+            var getDataJSON = JSON.stringify({
+                itemTagId: itemTagId
+            });
+            var deferred = $q.defer();
+            $http({
+                method: 'post',
+                url: url,
+                data: { json: getDataJSON }
+            }).success(
+                function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(
+                function (data, status, headers, config) {
+                    deferred.reject(5);
+                });
+            return deferred.promise;
+        }
+
+        //获取酒店列表数据
+        this.getHotelList = function (itemTagId) {
+            var url = baseUrl + 'app/menu/loadMenuChildList.do';
+            var getDataJSON = JSON.stringify({
+                itemTagId: itemTagId
+            });
+            var deferred = $q.defer();
+            $http({
+                method: 'post',
+                url: url,
+                data: { json: getDataJSON }
+            }).success(
+                function (data, status, headers, config) {
+                    deferred.resolve(data);
+                }).error(
+                function (data, status, headers, config) {
+                    deferred.reject(5);
+                });
+            return deferred.promise;
+        }
     }]);
--- TellMeApp/www/app/hotel/list/hotel-list-ctrl.js
+++ TellMeApp/www/app/hotel/list/hotel-list-ctrl.js
@@ -1,10 +1,78 @@
 ﻿angular.module('tellme')
-    .controller('hotelListControll',['$scope','$window', function ($scope, $window) {
+    .controller('hotelListControll', ['$scope', '$window', 'hotelSer', function ($scope, $window, hotelSer) {
+        var _MENU_SELECTED_ITEM = null;
+
         //后退
         $scope.$window = $window;
         $scope.go_back = function () {
             $window.history.back();
         };
+        //设置1级菜单选择事件
+        function setSelectStyle(obj, id) {
+            if (_MENU_SELECTED_ITEM != null) {
+                _MENU_SELECTED_ITEM.className = "";
+            }
+            obj.className = "li-but";
+            _MENU_SELECTED_ITEM = obj;
+            $scope.getChildMenu(id);
+        }
+
+        //获取1级标题
+        $scope.getRootMenu = function () {
+            var promise = hotelSer.getRootMenu();
+            promise.then(
+                function (data) {
+                    if (data.isSuccess) {
+                        var view = document.getElementById("list");
+                        view.innerHTML = "";
+                        var menus = data.rows;
+                        if (menus != null && menus.length > 0) {
+                            for (var i = 0; i < menus.length; i ++){
+                                var obj = menus[i];
+                                var item = document.createElement("li");
+                                item.innerHTML = obj.name;
+                                view.appendChild(item);
+                                new MenuItem(item, obj.itemTagId);
+                                //初始化选中
+                                if (i == 0) {
+                                    setSelectStyle(item, obj.itemTagId);
+                                }
+                            }
+                        }
+                    }
+                },
+                function (data) {
+                    console.log('其他');
+                }
+                );
+        }
+
+        //获取二级菜单
+        $scope.getChildMenu = function (itemTagId) {
+            var promise = hotelSer.getChildMenu(itemTagId);
+            promise.then(
+                function (data) {
+                    if (data.isSuccess) {
+                        var view = document.getElementById("child_menu_item_view");
+                        var menus = data.rows;
+                        var views = "";
+                        if (menus != null && menus.length > 0) {
+                            for (var i = 0; i < menus.length; i++) {
+                                var obj = menus[i];
+                                //加入样式后无法正常显示图片
+                                //views += "<div class=\"swiper-slide\"><a href=\"#\"><img src=\"" + hotelSer.hostUrl + obj.name + "\" /></a></div>";
+                                views += "<div><a href=\"#\"><img src=\"" + hotelSer.hostUrl + obj.name + "\" /></a></div>";
+                            }
+                        }
+                        view.innerHTML = views;
+                    }
+                },
+                function (data) {
+                    console.log('其他');
+                }
+                );
+        }
+
         ////页面初始加载
         var myScroll;
         var length = document.getElementById("list").getElementsByTagName("li").length;		//导航li的数量，若从后台读取可不要本行
@@ -18,4 +86,14 @@
             slidesPerView: 'auto',
         });
       
+        //设置1级标题
+        $scope.getRootMenu();
+
+        //Object
+        var MenuItem = function (object, id) {
+            //1级标题切换事件
+            object.onclick = function () {
+                setSelectStyle(object, id);
+            }
+        }
     }]);
