96e6fc410f3c5af5672788dbaafbf24abfdd27a1
SeeLittleGirlAgain <1309230548@qq.com>
2015-12-04 16:39:45 +0800

    提交

--- TellMeApp/www/app.js
+++ TellMeApp/www/app.js
@@ -58,7 +58,7 @@
             ;
             var appLaunchCount = window.localStorage.getItem('launchCount');
             if (appLaunchCount) {
-                $urlRouterProvider.otherwise('/home');
+                $urlRouterProvider.otherwise('/willSearch');
             } else {
                 $urlRouterProvider.otherwise('/start');
             }
