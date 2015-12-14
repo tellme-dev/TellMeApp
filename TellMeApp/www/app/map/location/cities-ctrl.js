efede250b025a29ffefd31b61a4e904db5e8991c
SeeLittleGirlAgain <1309230548@qq.com>
2015-12-14 20:58:54 +0800

    dingwei

--- TellMeApp/www/app/map/location/cities-ctrl.js
+++ TellMeApp/www/app/map/location/cities-ctrl.js
@@ -16,17 +16,63 @@
             name: '深圳',
             id: 3
         }, {
-            name: '广州',
+            name: '南昌',
             id: 4
         }, {
-            name: '成都',
+            name: '武汉',
             id: 5
         }, {
-            name: '深圳',
+            name: '长沙',
             id: 6
         }];
-        $scope.historicCities = $scope.hotSearchCities;
+        $scope.getRowArray = function (items) {
+            var array = [];
+            var count = 0;
+            var i = 0;
+            var j = 0;
+            while (count < items.length) {
+                if (count == 0) {
+                    array[i] = [];
+                    array[i][j++] = items[count];
+                } else if (count % 4 == 0) {
+                    i++;
+                    j = 0;
+                    array[i] = [];
+                    array[i][j++] = items[count];
+                } else {
+                    array[i][j++] = items[count];
+                }
+                count++;
+            }
+            return array;
+        }
 
+        $scope.hotArray = $scope.getRowArray($scope.hotSearchCities);
+
+        $scope.historicCities = $scope.hotSearchCities;
+        $scope.historicArray = $scope.getRowArray($scope.historicCities);
+
+        $scope.orderByAlpha = function (items) {
+            var orders = [];
+            var firstChar = items[0].firstChar;
+            var count = 0;
+            var i = 0;
+            var j = 0;
+            while (count < items.length) {
+                if (items[count].firstChar !== firstChar) {
+                    i++; j = 0;
+                    orders[i] = [];
+                } else {
+                    if (count == 0) {
+                        orders[i] = [];
+                    }
+                }
+                
+                orders[i][j++] = items[count];
+                count++;
+            }
+            return orders;
+        }
         $scope.regionlist = [
             {
                 id: 1,
@@ -55,6 +101,7 @@
                 firstChar: 'N'
             }
         ];
+        $scope.orderRegionList = $scope.orderByAlpha($scope.regionlist);
         $scope.goBack = function () {
             $ionicHistory.goBack();
         }
@@ -81,8 +128,29 @@
                 $scope.hasInputSearchText = true;
             }
         }
+
         $scope.changeRegion = function (region) {
-            window.localStorage['currentcity'] = name;
-            $scope.currentCity = window.localStorage['currentcity'];
+            window.localStorage['currentcity'] = region.name;
+            $scope.currentCity = region.name;
+            $scope.searchText = '';
+        }
+        $scope.deleteHistoricItem = function (item) {
+            //删除元素
+            $scope.historicCities = $scope.remove($scope.historicCities, item);
+            $scope.historicArray = $scope.getRowArray($scope.historicCities);
+        }
+        $scope.remove = function (items, item) {
+            if (item.name == window.localStorage['currentcity']) {
+                tellMeMapSvr.updateCurrentcity();
+                $scope.currentCity = window.localStorage['currentcity'];
+            }
+            var i = 0;
+            for (i = 0; i < items.length; i++) {
+                if (item.name == items[i].name) {
+                    break;
+                }
+            }
+            items.splice(i, i + 1);
+            return items;
         }
     }])
--- TellMeApp/www/app/util/tellmeDirective.js
+++ TellMeApp/www/app/util/tellmeDirective.js
@@ -1,19 +1,94 @@
 ﻿angular.module('tellme')
-    .directive('hot', function () {
+    .directive('hotregion',function(){
+        return {
+            restrict: 'EA',
+            scope: {
+                region: '=',
+                index: '=',
+                change: '&',
+                length:'='
+            },
+            template: function (element, attrs) {
+                var html = '<div ng-if="index % 4 === 0" class="row dw-row" style="margin-top:10px;"><button ng-if="index % 4 === 0" class="col col-20" ng-click="change()">{{region.name}}</button>'
+                + '<button ng-if="index % 4 === 3 || index === length" class="col col-20" ng-click="change()">{{region.name }}</button></div>'
+                + '<button ng-if="index % 4 !=0&&index % 4 !== 3 && index !== length" class="col col-20" ng-click="change()">{{region.name}}</button>';
+                return html;
+            },
+            link: function (scope, element, attrs) {
+                
+            }
+        }
+    })
+        .directive('hot', function () {
+            return {
+                restrict: 'AE',
+                scope: {
+                    hzf: "&",
+                    hotsearchregions: '='
+                },
+                link: function (scope, element, attrs) {
+                    var html = '';
+                    var hotSearchRegions = scope.hotsearchregions;
+
+                    angular.forEach(hotSearchRegions, function (region, index) {
+                        if (index % 4 === 0) {
+                            html += '<div class="row dw-row" style="margin-top:10px;"><button class="col col-20" ng-click="hzf()">' + region.name + '</button>';
+
+                        } else if (index % 4 === 3 || index === hotSearchRegions.length) {
+                            html += '<button class="col col-20" ng-click="hzf()">' + region.name + '</button></div>';
+                        } else {
+                            html += '<button class="col col-20" ng-click="hzf()">' + region.name + '</button>';
+                        }
+                    }
+                        );
+                    element.replaceWith(html);
+                    scope.hzf = function () {
+                        alert('hello');
+                    }
+                }
+            };
+        })
+     .directive('historic', function () {
+         return {
+             restrict: 'AE',
+             link: function (scope, element, attrs) {
+                 var html = '';
+                 var historicRegions = scope[attrs.ngModel];
+
+                 angular.forEach(historicRegions, function (region, index) {
+                     if (index % 4 === 0) {
+                         html += '<div ng-if="" class="row dw-row-sr" style="margin-top:10px;"><button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button>';
+
+                     } else if (index % 4 === 3 || index === historicRegions.length) {
+                         html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button></div>';
+                     } else {
+                         html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button>';
+                     }
+                 }
+                     );
+                 element.replaceWith(html);
+             }
+         };
+     })
+    .directive('regionls', function () {
         return {
             restrict: 'AE',
             link: function (scope, element, attrs) {
                 var html = '';
-                var hotSearchRegions = scope[attrs.ngModel];
-
-                angular.forEach(hotSearchRegions, function (region, index) {
-                    if (index % 4 === 0) {
-                        html += '<div ng-if="" class="row dw-row" style="margin-top:10px;"><button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '</button>';
-
-                    } else if (index % 4 === 3 || index === hotSearchRegions.length) {
-                        html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '</button></div>';
-                    } else {
-                        html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '</button>';
+                var regionList = scope[attrs.ngModel];
+                var firstChar = angular.uppercase(regionList[0].firstChar);
+                angular.forEach(regionList, function (region, index) {
+                    var tempChar = angular.uppercase(region.firstChar);
+                    if (index == 0) {
+                        html += '<div class="list dw-list"><div class="item item-divider">' + firstChar + '</div>';
+                    }
+                    if (tempChar != firstChar) {
+                        firstChar = tempChar;
+                        html += '<div class="item item-divider">' + firstChar + '</div>';
+                    }
+                    html += '<a class="item" href="#">' + region.name + '</a>';
+                    if (index == regionList.length - 1) {
+                        html += '</div>';
                     }
                 }
                     );
@@ -21,51 +96,3 @@
             }
         };
     })
- .directive('historic', function () {
-     return {
-         restrict: 'AE',
-         link: function (scope, element, attrs) {
-             var html = '';
-             var historicRegions = scope[attrs.ngModel];
-
-             angular.forEach(historicRegions, function (region, index) {
-                 if (index % 4 === 0) {
-                     html += '<div ng-if="" class="row dw-row-sr" style="margin-top:10px;"><button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button>';
-
-                 } else if (index % 4 === 3 || index === historicRegions.length) {
-                     html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button></div>';
-                 } else {
-                     html += '<button class="col col-20" ng-click="changeRegion(' + region.name + ')">' + region.name + '<i style="float:right;width:1.125em; height:1.125em;border-radius:50%; background:#686161;text-align:top;line-height:1.125em;color:#fff;display:inline-block;left:40px; top:15px;">×</i></button>';
-                 }
-             }
-                 );
-             element.replaceWith(html);
-         }
-     };
- })
-.directive('regionls', function () {
-    return {
-        restrict: 'AE',
-        link: function (scope, element, attrs) {
-            var html = '';
-            var regionList = scope[attrs.ngModel];
-            var firstChar = angular.uppercase(regionList[0].firstChar);
-            angular.forEach(regionList, function (region, index) {
-                var tempChar = angular.uppercase(region.firstChar);
-                if (index == 0) {
-                    html += '<div class="list dw-list"><div class="item item-divider">' + firstChar + '</div>';
-                }
-                if (tempChar != firstChar) {
-                    firstChar = tempChar;
-                    html+='<div class="item item-divider">' + firstChar + '</div>';
-                }
-                html += '<a class="item" href="#">' + region.name + '</a>';
-                if (index == regionList.length - 1) {
-                    html += '</div>';
-                }
-            }
-                );
-            element.replaceWith(html);
-        }
-    };
-})
