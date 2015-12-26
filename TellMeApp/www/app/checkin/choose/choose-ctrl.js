cbba63d60f6720447e7a57af01f2734489c4e63b
SeeLittleGirlAgain <1309230548@qq.com>
2015-12-26 16:47:59 +0800

    入住

---
+++ TellMeApp/www/app/checkin/choose/choose-ctrl.js
@@ -0,0 +1,13 @@
+﻿angular.module('tellme')
+    .controller('chooseControll', ['$scope', '$state', '$ionicHistory', '$stateParams', 'appConfig', function ($scope, $state, $ionicHistory,$stateParams, appConfig) {
+        $scope.baseUrl = appConfig.server.getUrl();
+        $scope.item = angular.fromJson($stateParams.item);
+
+        $scope.goBack = function () {
+            $ionicHistory.goBack();
+        }
+        $scope.call = function () {
+            //拨打电话
+            //item.tel;
+        }
+    }])
