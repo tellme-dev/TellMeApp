5b8e3b32dbda1cddc336c8910b22af1e3a08f799
jun1943 <992717311@qq.com>
2016-01-05 14:50:35 +0800

    gengxin

---
+++ TellMeApp/www/app/util/app-config.js
@@ -0,0 +1,32 @@
+﻿
+/*
+app 配置信息
+包括: 
+后台服务器地址
+*/
+angular.module('tellme')
+    .constant('appConfig',
+    {
+        /*
+        后台服务配置
+        */
+        server: {
+            //address: 'http://112.74.209.133',
+            address: 'http://192.168.1.104',
+            //name: 'tellme',
+            name: 'TellMeMgr',
+            port: '8080',
+            getUrl: function () {
+                return this.address + ':' + this.port + '/' + this.name + '/';
+
+            }
+        },
+
+    });
+
+/*
+
+
+
+
+*/
